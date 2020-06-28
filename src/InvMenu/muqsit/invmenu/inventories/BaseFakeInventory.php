b8b69e21b6aa355c846a6526454cd837bd00f03a
ZulfahmiFjr <41950923+ZulfahmiFjr@users.noreply.github.com>
2020-06-28 11:23:03 +0700

    Update BaseFakeInventory.php

--- src/InvMenu/muqsit/invmenu/inventories/BaseFakeInventory.php
+++ src/InvMenu/muqsit/invmenu/inventories/BaseFakeInventory.php
@@ -56,7 +56,7 @@ class BaseFakeInventory extends ContainerInventory{
 	}
 
 	public function createNewInstance(InvMenu $menu) : BaseFakeInventory{
-		return new BaseFakeInventory($menu, $this->getContents());
+		return new static($menu, $this->getContents());
 	}
 
 	final public function send(Player $player, ?string $custom_name) : bool{
