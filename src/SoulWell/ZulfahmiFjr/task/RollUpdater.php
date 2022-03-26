d9c7ccbeeff3280650b378b9f640406f78b6bf79
ZulfahmiFjr <41950923+ZulfahmiFjr@users.noreply.github.com>
2022-03-26 13:09:22 +0700

    Update RollUpdater.php

--- src/SoulWell/ZulfahmiFjr/task/RollUpdater.php
+++ src/SoulWell/ZulfahmiFjr/task/RollUpdater.php
@@ -153,10 +153,7 @@ class RollUpdater extends Task{
       }
       if($delay === -15){
        $item = $wellInventory->getItem(31);
-       if(($p instanceof Player) && ($p->isOnline())){
-        $this->addItemToPlayer($p, $item);
-        $p->sendMessage("§f§lSoulWell§r§f: §7§oYou get §r§f".$item->getName()." §7§owith amount §r§f".$item->getCount()." §7§ofrom SoulWell§r§f.");
-       }
+       
        $this->getHandler()->cancel();
        $wellInventory->close($p);
       }
