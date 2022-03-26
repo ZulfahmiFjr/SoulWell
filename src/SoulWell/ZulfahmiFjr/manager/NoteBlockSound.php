cc1ef47b0c10f47796bd68b2077ffdd7f42af3bf
ZulfahmiFjr <41950923+ZulfahmiFjr@users.noreply.github.com>
2022-03-26 11:02:26 +0700

    Update NoteBlockSound.php

--- src/SoulWell/ZulfahmiFjr/manager/NoteBlockSound.php
+++ src/SoulWell/ZulfahmiFjr/manager/NoteBlockSound.php
@@ -3,46 +3,29 @@
 namespace SoulWell\ZulfahmiFjr\manager;
 
 use pocketmine\world\sound\Sound;
-
 use pocketmine\math\Vector3;
-
 use pocketmine\network\mcpe\protocol\BlockEventPacket;
-
 use pocketmine\network\mcpe\protocol\types\BlockPosition;
-
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 
 class NoteBlockSound implements Sound{
 
     private $note;
-
+    
     public function __construct($note){
-
      $this->note = $note;
-
     }
 
     public function encode(Vector3 $vector):array{
-
      $pk = new BlockEventPacket();
-
      $pk->blockPosition = new BlockPosition($vector->x, $vector->y, $vector->z);
-
      $pk->eventType = 0;
-
      $pk->eventData = $this->note;
-
      $pk2 = new LevelSoundEventPacket();
-
      $pk2->sound = 81;
-
      $pk2->position = $vector;
-
      $pk2->extraData = 0 | $this->note;
-
      return [$pk, $pk2];
-
     }
 
 }
-
