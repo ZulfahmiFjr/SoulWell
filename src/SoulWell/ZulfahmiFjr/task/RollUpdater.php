a5be5f7a2392e4c078e3dfaf86a740f44929c2c7
ZulfahmiFjr <41950923+ZulfahmiFjr@users.noreply.github.com>
2022-03-26 12:12:27 +0700

    Update RollUpdater.php

--- src/SoulWell/ZulfahmiFjr/task/RollUpdater.php
+++ src/SoulWell/ZulfahmiFjr/task/RollUpdater.php
@@ -43,13 +43,13 @@ class RollUpdater extends Task{
        $item = $this->getReward();
        if($item !== null){
         if($p->isOnline()){
-         $p->getInventory()->addItem($item);
+         $this->addItemToPlayer($p, $item);
          $p->sendMessage("§f§lSoulWell§r§f: §7§oYou get §r§f".$item->getName()." §7§owith amount §r§f".$item->getCount()." §7§ofrom SoulWell§r§f.");
         }
        }
       }else if(($item = $this->prediction) instanceof Item){
        if($p->isOnline()){
-        $p->getInventory()->addItem($item);
+        $this->addItemToPlayer($p, $item);
         $p->sendMessage("§f§lSoulWell§r§f: §7§oYou get §r§f".$item->getName()." §7§owith amount §r§f".$item->getCount()." §7§ofrom SoulWell§r§f.");
        }
       }
@@ -150,13 +150,17 @@ class RollUpdater extends Task{
       if($delay === -15){
        $item = $wellInventory->getItem(31);
        if($p->isOnline()){
-        $p->getInventory()->addItem($item);
+        $this->addItemToPlayer($p, $item);
         $p->sendMessage("§f§lSoulWell§r§f: §7§oYou get §r§f".$item->getName()." §7§owith amount §r§f".$item->getCount()." §7§ofrom SoulWell§r§f.");
        }
        $this->getHandler()->cancel();
        $wellInventory->close($p);
       }
      }
+    }
+
+    private function addItemToPlayer(Player $p, Item $item):void{
+     $p->getInventory()->addItem($item);
     }
 
 }
